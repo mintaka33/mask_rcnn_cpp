9520f69c5d28e36473788921cad29487ebcbffad
mintaka33 <mintaka33@outlook.com>
2018-12-13 16:22:58 +0800

    add interface

--- segment/segment/segment.cpp
+++ segment/segment/segment.cpp
@@ -1,10 +1,58 @@
 #include "segment.h"
 
+vector<string> classes = { 
+    "person","bicycle","car","motorcycle","airplane","bus","train","truck","boat","traffic light",
+    "fire hydrant","","stop sign","parking meter","bench","bird","cat","dog","horse","sheep","cow","elephant",
+    "bear","zebra","giraffe","","backpack","umbrella","","","handbag","tie","suitcase","frisbee","skis",
+    "snowboard","sports ball","kite","baseball bat","baseball glove","skateboard","surfboard","tennis racket",
+    "bottle","","wine glass","cup","fork","knife","spoon","bowl","banana","apple","sandwich","orange","broccoli",
+    "carrot","hot dog","pizza","donut","cake","chair","couch","potted plant","bed","","dining table","","","toilet",
+    "","tv","laptop","mouse","remote","keyboard","cell phone","microwave","oven","toaster","sink","refrigerator","",
+    "book","clock","vase","scissors","teddy bear","hair drier","toothbrush"
+};
+
 FrameSegment::FrameSegment()
 {
 }
 
 FrameSegment::~FrameSegment()
 {
+}
+
+int FrameSegment::init(string m, string c)
+{
+    model = m;
+    config = c;
+    loadNet();
+
+    return 0;
+}
+
+int FrameSegment::init()
+{
+    model = "frozen_inference_graph.pb";
+    config = "mask_rcnn_inception_v2_coco_2018_01_28.pbtxt";
+    loadNet();
+
+    return 0;
+}
+
+int FrameSegment::segment()
+{
+    return 0;
+}
+
+int FrameSegment::destroy()
+{
+    return 0;
+}
+
+int FrameSegment::loadNet()
+{
+    Net net = readNet(model, config, framework);
+    net.setPreferableBackend(backendId);
+    net.setPreferableTarget(targetId);
+    
+    return 0;
 }
 
--- segment/segment/segment.h
+++ segment/segment/segment.h
@@ -5,13 +5,37 @@
 #include <iostream>
 #include <string.h>
 
+#include <opencv2/dnn.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/highgui.hpp>
+
+using namespace cv;
+using namespace dnn;
+using namespace std;
+
 class FrameSegment
 {
 public:
     FrameSegment();
     ~FrameSegment();
 
+    int init();
+    int init(string model, string config);
+    int segment();
+    int destroy();
 private:
+    int loadNet();
 
+private:
+    float confThreshold = (float) 0.9; // Confidence threshold
+    float maskThreshold = (float) 0.3; // Mask threshold
+    string model;
+    string config;
+    string framework = "";
+    int backendId = 0;
+    int targetId = 0;
+    float scale = (float)1.0; // (1/255)
+    Scalar mean = { 0.0, 0.0, 0.0, 0.0 };
+    bool swapRB = true;
 };
 
